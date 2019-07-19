80b17c7b3ac44042e2aef638db0577f862455dea
Mohammad Sorowar Hossain <mhoss147@my.centennialcollege.ca>
2019-07-18 20:32:48 -0400

    Created Main Form

--- COMP123-S2019-Lab10-300763479/MainForm.cs
+++ COMP123-S2019-Lab10-300763479/MainForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace COMP123_S2019_Lab10_300763479
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
